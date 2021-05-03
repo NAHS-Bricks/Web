ef1803de0c24b237d8cdd2754f94708a849e26b7
Nils Ost <home@nijos.de>
2021-05-03 22:26:30 +0200

    made human readable stuff a bit nicer

--- helpers/template.py
+++ helpers/template.py
@@ -1,6 +1,7 @@
 import json
 from helpers.brickserver import temp_sensor_get, latch_get
 from helpers.shared import config
+from datetime import datetime, timedelta
 
 
 def convert_html(json_obj):
@@ -32,3 +33,16 @@ def grafana_url_latch(latch_id):
     url = "http://" + config['grafana']['host'] + ":" + str(config['grafana']['port']) + "/explore"
     query = f"?orgId=1&left=%5B%22now-6h%22,%22now%22,%22{config['grafana']['datasource']}%22,%7B%22datasource%22:%22{config['grafana']['datasource']}%22,%22policy%22:%2226weeks%22,%22resultFormat%22:%22time_series%22,%22orderByTime%22:%22ASC%22,%22tags%22:%5B%7B%22key%22:%22brick_id%22,%22operator%22:%22%3D%22,%22value%22:%22{brick_id}%22%7D,%7B%22condition%22:%22AND%22,%22key%22:%22latch_id%22,%22operator%22:%22%3D%22,%22value%22:%22{lid}%22%7D%5D,%22groupBy%22:%5B%5D,%22select%22:%5B%5B%7B%22type%22:%22field%22,%22params%22:%5B%22state%22%5D%7D%5D%5D,%22measurement%22:%22latches%22%7D%5D"
     return url + query
+
+
+def time_ago_str(fromtime_ts):
+    seconds = (datetime.now() - datetime.fromtimestamp(fromtime_ts)).seconds
+    result = ''
+    if seconds > (60 * 60):
+        result += str(int(seconds / (60 * 60))) + 'h '
+        seconds %= (60 * 60)
+    if seconds > 60:
+        result += str(int(seconds / 60)) + 'm '
+        seconds %= 60
+    result += str(seconds) + 's'
+    return result
