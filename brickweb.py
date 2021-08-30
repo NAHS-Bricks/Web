c9e8ee9b3f2bd18bf76a1f5c8a562b0e5c462444
Nils Ost <home@nijos.de>
2021-08-30 14:23:11 +0200

    removed Event-System

--- brickweb.py
+++ brickweb.py
@@ -7,9 +7,6 @@ from helpers.brickserver import features_get_available, clear_request_cache
 from helpers.brickserver import temp_sensor_exists, temp_sensor_get, temp_sensor_set_desc, temp_sensor_disable
 from helpers.brickserver import latch_exists, latch_get, latch_set_desc, latch_set_states_desc, latch_add_trigger, latch_del_trigger, latch_disable
 from helpers.brickserver import signal_exists, signal_get, signal_set_desc, signal_set_states_desc, signal_set_state, signal_disable
-from helpers.brickserver import event_get, event_add, event_delete, event_set_pos
-from helpers.brickserver import event_command_set, event_reaction_add, event_reaction_delete, event_reaction_move
-from helpers.brickserver import event_data_replace, event_data_get, event_data_get_names
 from helpers.shared import config, serve_template, possible_disables
 from helpers.template import server_is
 
@@ -34,8 +31,6 @@ class BrickWeb(object):
         for val in ['bricks_filter_feature', 'bricks_filter_string']:
             if val not in cherrypy.session:
                 cherrypy.session[val] = None
-        if 'event_id' not in cherrypy.session:
-            cherrypy.session['event_id'] = None
 
         if 'show_disabled' not in cherrypy.session:
             cherrypy.session['show_disabled'] = False
@@ -172,113 +167,6 @@ class BrickWeb(object):
     def toggle_show_disabled(self):
         cherrypy.session['show_disabled'] = not cherrypy.session['show_disabled']
         raise cherrypy.HTTPRedirect('/')
-
-    @cherrypy.expose()
-    def event_editor(self, subpath=None, event_id=None, reaction_pos=None, reaction_name=None, command_name=None, ed_level=None, ed_name=None, ed_name_new=None, ed_data=None):
-        if not server_is('0.6.0'):
-            raise cherrypy.HTTPRedirect('/')
-        brick = brick_get(cherrypy.session['brick_id'])
-        if cherrypy.session['event_id'] is not None:
-            event = event_get(cherrypy.session['event_id'])
-        else:
-            event = None
-        if subpath is None:
-            return serve_template('/events/editor.html', brick=brick, event=event)
-        elif subpath == 'add_event':
-            event_add(cherrypy.session['brick_id'])
-            brick = brick_get(cherrypy.session['brick_id'])
-            return serve_template('/events/column-events.html', brick=brick)
-        elif subpath == 'delete_event' and event_id is not None:
-            event_delete(cherrypy.session['brick_id'], event_id)
-            if event_id == cherrypy.session['event_id']:
-                cherrypy.session['event_id'] = None
-                event = None
-            brick = brick_get(cherrypy.session['brick_id'])
-            return serve_template('/events/editor.html', brick=brick, event=event)
-        elif subpath == 'move_up_event' and event_id is not None:
-            event_set_pos(brick['_id'], event_id, brick['events'].index(event_id) - 1)
-            brick = brick_get(cherrypy.session['brick_id'])
-            return serve_template('/events/column-events.html', brick=brick)
-        elif subpath == 'move_down_event' and event_id is not None:
-            event_set_pos(brick['_id'], event_id, brick['events'].index(event_id) + 1)
-            brick = brick_get(cherrypy.session['brick_id'])
-            return serve_template('/events/column-events.html', brick=brick)
-        elif subpath == 'select_event' and event_id is not None:
-            cherrypy.session['event_id'] = event_id
-            event = event_get(event_id)
-            return serve_template('/events/editor.html', brick=brick, event=event)
-        elif subpath == 'new_reaction':
-            return serve_template('/events/reaction.html', event=event, reaction_pos=None, reaction=None, event_data=None)
-        elif subpath == 'save_reaction' and reaction_pos is not None and reaction_name is not None and ed_level is not None and ed_name is not None and ed_name_new is not None and ed_data is not None:
-            if ed_name == '_new_':
-                ed_name = ed_name_new
-            elif ed_name == '_auto_':
-                auto_c = 0
-                for name in event_data_get_names(event['_id'], ed_level):
-                    if name.startswith('auto'):
-                        auto_c += 1
-                while 'auto_' + str(auto_c) in event_data_get_names(event['_id'], ed_level):
-                    auto_c += 1
-                ed_name = 'auto_' + str(auto_c)
-            if ed_level == 'b':
-                ed_name = '_' + ed_name
-            elif ed_level == 'g':
-                ed_name = '__' + ed_name
-            ed_data = json.loads(ed_data)
-            event_data_replace(event['_id'], ed_name, ed_data)
-            event_reaction_add(event['_id'], reaction_name, ed_name)
-            if not reaction_pos == 'None':
-                reaction_pos = int(reaction_pos)
-                event_reaction_delete(event['_id'], reaction_pos)
-                event_reaction_move(event['_id'], len(event['reactions']) - 1, reaction_pos)
-            event = event_get(event['_id'])
-            return serve_template('/events/column-reactions.html', brick=brick, event=event)
-        elif subpath == 'save_command' and command_name is not None and ed_level is not None and ed_name is not None and ed_name_new is not None and ed_data is not None:
-            if ed_name == '_new_':
-                ed_name = ed_name_new
-            elif ed_name == '_auto_':
-                auto_c = 0
-                for name in event_data_get_names(event['_id'], ed_level):
-                    if name.startswith('auto'):
-                        auto_c += 1
-                while 'auto_' + str(auto_c) in event_data_get_names(event['_id'], ed_level):
-                    auto_c += 1
-                ed_name = 'auto_' + str(auto_c)
-            if ed_level == 'b':
-                ed_name = '_' + ed_name
-            elif ed_level == 'g':
-                ed_name = '__' + ed_name
-            ed_data = json.loads(ed_data)
-            event_data_replace(event['_id'], ed_name, ed_data)
-            event_command_set(event['_id'], command_name, ed_name)
-            event = event_get(event['_id'])
-            return serve_template('/events/column-command.html', brick=brick, event=event)
-        elif subpath == 'delete_reaction' and reaction_pos is not None:
-            if not reaction_pos == 'None':
-                reaction_pos = int(reaction_pos)
-                event_reaction_delete(event['_id'], reaction_pos)
-            event = event_get(event['_id'])
-            return serve_template('/events/column-reactions.html', brick=brick, event=event)
-        elif subpath == 'move_up_reaction' and reaction_pos is not None:
-            reaction_pos = int(reaction_pos)
-            event_reaction_move(event['_id'], reaction_pos, reaction_pos - 1)
-            event = event_get(event['_id'])
-            return serve_template('/events/column-reactions.html', brick=brick, event=event)
-        elif subpath == 'move_down_reaction' and reaction_pos is not None:
-            reaction_pos = int(reaction_pos)
-            event_reaction_move(event['_id'], reaction_pos, reaction_pos + 1)
-            event = event_get(event['_id'])
-            return serve_template('/events/column-reactions.html', brick=brick, event=event)
-        elif subpath == 'event_data_data' and ed_level is not None and ed_name is not None:
-            if ed_name in ['_new_', '_auto_']:
-                return 'None'
-            if ed_level == 'b':
-                ed_name = '_' + ed_name
-            elif ed_level == 'g':
-                ed_name = '__' + ed_name
-            return json.dumps({k: v for k, v in event_data_get(event['_id'], ed_name).items() if k != '_id'})
-        else:
-            raise cherrypy.HTTPRedirect('/')
 
 
 if __name__ == '__main__':
