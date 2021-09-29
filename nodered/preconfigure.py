ba6fea42cac41138dd3017034dd6af077cbe6a85
Nils Ost <home@nijos.de>
2021-09-29 23:00:16 +0200

    saving

--- nodered/preconfigure.py
+++ nodered/preconfigure.py
@@ -1,7 +1,25 @@
 import requests
 import json
 
+config = {
+    'host': '',
+    'port': 3000,
+    'mqtt_host': '',
+    'mqtt_port': 1883,
+    'brickserver_host': '',
+    'brickserver_port': 8081
+}
+
+mqtt_node_id = '6c8bc1031ca33a4f'
+brickserver_node_id = '80ae1dc59aca1268'
+
 flows = json.loads(open('flows.json', 'r').read())
+
+for node in flows:
+    if node['id'] == brickserver_node_id:
+        pass
+    elif node['id'] == mqtt_node_id:
+        pass
 
 server = "http://192.168.1.232:1880"
 s = requests.Session()
